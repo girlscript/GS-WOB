fcb7d8491ae345ce926f1d62a77f4395ed901a0b
Hemu21 <rajva5727@gmail.com>
2024-09-02 13:08:55 +0530

    added github
    Co-authored-by: Hemu21 <rajva5727@gmail.com>
    Co-authored-by: sanjay-kv <sanjay.kanakkot@gmail.com>

--- components/homepage/SocialFollow.js
+++ components/homepage/SocialFollow.js
@@ -9,26 +9,16 @@ import {
   faXTwitter,
   faInstagram,
   faLinkedin,
+  faGithub
 } from "@fortawesome/free-brands-svg-icons";
-import { useRouter } from "next/router";
 
 export default function SocialFollow() {
-  const router = useRouter();
-  const [isWobRoute, setIsWobRoute] = useState(false);
-
-  useEffect(() => {
-    if (router.asPath.startsWith("/wob")) {
-      setIsWobRoute(true); // set it as true if you want to launch the website
-    } else {
-      setIsWobRoute(false);
-    }
-  }, [router.asPath]);
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
   return (
     <>
-      {isWobRoute ? (
+      {(
         <div
           data-aos="right"
           className="fixed object-right text-xs text-right right-0 pr-4 pl-4 hidden sm:block z-50"
@@ -73,64 +63,13 @@ export default function SocialFollow() {
           >
             <FontAwesomeIcon className="w-10 h-10" icon={faYoutube} size="2x" />
           </a>
-        </div>
-      ) : (
-        <div
-          data-aos="right"
-          className="fixed object-right text-xs text-right right-0 pr-4 pl-4 hidden sm:block z-50"
-        >
-          {/* <h3 className="text-white">Social Follow</h3> */}
           <a
-            href="https://www.facebook.com/girlscriptsoc/"
+            href="https://github.com/Girlscript-WOB"
             target="_blank"
-            className="dark:transition-colors transition-colors bg-transparent dark:text-[#ffff] dark:hover:text-facebook-0 dark:ease-linear dark:hover:ease-in block text-center text-gray-700 py-2 px-2 text-base hover:text-blue-600 ease-linear hover:ease-in rounded-t-md"
+            className="dark:transition-colors transition-colors  bg-transparent dark:text-[#ffff] dark:hover:text-black dark:ease-linear dark:hover:ease-in block text-center text-gray-700 py-2 px-2 text-base  hover:text-black ease-linear hover:ease-in rounded-b-md"
             rel="noreferrer"
           >
-            <FontAwesomeIcon
-              className="w-10 h-10"
-              icon={faFacebook}
-              size="2x"
-            />
-          </a>
-          <a
-            href="https://www.instagram.com/girlscriptsummerofcode/?hl=en"
-            target="_blank"
-            className="dark:transition-colors transition-colors  bg-transparent dark:text-[#ffff] dark:hover:text-pink-600 dark:ease-linear dark:hover:ease-in block text-center text-gray-700 py-2 px-2 text-base  hover:text-pink-600 ease-linear hover:ease-in"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon
-              className="w-10 h-10"
-              icon={faInstagram}
-              size="2x"
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/company/girlscriptsoc/mycompany/"
-            target="_blank"
-            className="dark:transition-colors transition-colors  bg-transparent dark:text-[#ffff] dark:hover:text-linkedin-0 dark:ease-linear dark:hover:ease-in block text-center text-gray-700 py-2 px-2 text-base  hover:text-blue-700 ease-linear hover:ease-in"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon
-              className="w-10 h-10"
-              icon={faLinkedin}
-              size="2x"
-            />
-          </a>
-          <a
-            href="https://x.com/girlscriptsoc"
-            target="_blank"
-            className="dark:transition-colors transition-colors  bg-transparent dark:text-[#ffff] dark:hover:text-twitter-0 dark:ease-linear dark:hover:ease-in block text-center text-gray-700 py-2 px-2 text-base  hover:text-blue-400 ease-linear hover:ease-in"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon className="w-10 h-10" icon={faXTwitter} size="2x" />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCBOlJtDcWNh0aUkS2CfI8Aw"
-            target="_blank"
-            className="dark:transition-colors transition-colors  bg-transparent dark:text-[#ffff] dark:hover:text-youtube-0 dark:ease-linear dark:hover:ease-in block text-center text-gray-700 py-2 px-2 text-base  hover:text-red-600 ease-linear hover:ease-in rounded-b-md"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon className="w-10 h-10" icon={faYoutube} size="2x" />
+            <FontAwesomeIcon className="w-10 h-10" icon={faGithub} size="2x" />
           </a>
         </div>
       )}
